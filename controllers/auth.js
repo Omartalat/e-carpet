2760d5c22aa83b62828eb57bbe6a45673f08e2b0
dr.omartalat <dr.omartalat@gmail.com>
2025-01-05 07:29:31 +0200

    adding authentication to reset password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,22 @@
+const crypto = require("crypto");
+
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 require("dotenv").config();
 // Import the Nodemailer library
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const { buffer } = require("stream/consumers");
+const { constrainedMemory } = require("process");
 
 // Create a transporter object
 const transporter = nodemailer.createTransport({
-  host: 'live.smtp.mailtrap.io',
+  host: "live.smtp.mailtrap.io",
   port: 587,
   secure: false, // use SSL
   auth: {
     user: process.env.EMAIL_API_USER,
     pass: process.env.EMAIL_API_PASS,
-  }
+  },
 });
 
 exports.getLogin = (req, res, next) => {
@@ -120,18 +124,18 @@ exports.postSignup = async (req, res, next) => {
     await newUser.save();
 
     const mailOptions = {
-      from: 'demo@demomailtrap.com',
-      to: 'dr.omartalat@gmail.com',
-      subject: 'Sending Email using Node.js',
-      text: 'That was easy!'
+      from: "demo@demomailtrap.com",
+      to: "dr.omartalat@gmail.com",
+      subject: "Account created",
+      text: "That was easy!",
     };
-    
+
     // Send the email
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         console.log(error);
       } else {
-        console.log('Email sent: ' + info.response);
+        console.log("Email sent: " + info.response);
       }
     });
 
@@ -162,4 +166,111 @@ exports.getReset = (req, res, next) => {
     isAuthenticated: false,
     errorMessage: req.errorMessage,
   });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, async (err, buffer) => {
+    if (err) {
+      res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    try {
+      const user = await User.findOne({ email: req.body.email });
+      if (!user) {
+        return res.render("auth/reset", {
+          path: "/reset",
+          pageTitle: "Reset Password",
+          isAuthenticated: false,
+          errorMessage: "No account with that email found!",
+        });
+      }
+      user.resetToken = token;
+      user.resetTokenExpiration = Date.now() + 3600000;
+      await user.save();
+      const mailOptions = {
+        from: "demo@demomailtrap.com",
+        to: "dr.omartalat@gmail.com",
+        subject: "Password reset",
+        html: `
+        <p>You requested a password reset<p>
+        <p>Click this <a href="http://localhost:3000/reset/${token}" >link<a> to reset your password<p>
+        `,
+      };
+
+      // Send the email
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      res.status(500).render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        isAuthenticated: false,
+        errorMessage: "An error occurred. Please try again later.",
+        token: token,
+        userId: userId,
+      });
+    }
+  });
+};
+
+exports.getNewPassword = async (req, res, next) => {
+  const token = req.params.token;
+  try {
+    const user = await User.findOne({
+      resetToken: token,
+      resetTokenExpiration: { $gt: Date.now() },
+    });
+    res.render("auth/new-password", {
+      path: "/new-password",
+      pageTitle: "New Password",
+      isAuthenticated: false,
+      errorMessage: req.errorMessage,
+      token: token,
+      userId: user._id.toString(),
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.postNewPassword = async (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const token = token;
+  try {
+    const restUser = await User.findOne({resetToken: token, resetTokenExpiration: { $gt: Date.now() }, _id: userId})
+      if (!restUser) {
+        return res.status(400).render("auth/new-password", {
+          path: "/new-password",
+          pageTitle: "New Password",
+          isAuthenticated: false,
+          errorMessage: "Invalid or expired token.",
+        });
+      }
+
+      restUser.password = await bcrypt.hash(newPassword, 12);
+      restUser.resetToken = undefined;
+      restUser.resetTokenExpiration = undefined;
+
+      await restUser.save();
+
+      res.redirect('/login');
+
+      restUser.password = await bcrypt.hash(newPassword, 12);
+      restUser.resetToken = undefined;
+      restUser.resetTokenExpiration = undefined;
+
+      await restUser.save();
+
+    res.redirect('/login');
+  } catch (err) {
+    console.log(err);
+  }
 };
